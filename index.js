794ddfedecae0b541117099de0e178114d852abc
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-07-09 10:56:16 +0300

    feat: slider

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+// swiper
 const swiper = new Swiper('.swiper', {
   loop: true,
   slidesPerView: 3,
-  spaceBetween: 30,
+  spaceBetween: 90,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+const sliders = document.querySelectorAll('.swiper-slide')
+
+sliders.forEach((slider)=>{
+  slider.setAttribute('style', 'width: 270px; margin-right: 90px;')
+})
+
+
+// scroll
+const anchors = document.querySelectorAll('a.scroll-to')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href')
+
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
